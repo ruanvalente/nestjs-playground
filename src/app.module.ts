49f3049864fa0b19b98bce92253c06a06c8accbf
ruanvalente <ruanopensuselinux@gmail.com>
2021-01-13 20:59:44 -0300

    feat: created entity for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { UserService } from './user/services/user.service';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { UserController } from './user/controllers/user.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { UserController } from './user/controllers/user.controller';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/ typeorm.config';
 
 @Module({
-  imports: [AppModule],
+  imports: [AppModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
